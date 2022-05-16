68b1c206aa29d878de5136b724f7ecc75549e57e
omar khaled <omarkhald02@gmail.com>
2022-05-16 21:26:25 +0200

    Time: 520 ms (95.88%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-select employee_id  from Employees
+/*select employee_id  from Employees
 where employee_id not in 
 ( select e.employee_id 
 from Employees as e   join Salaries as s
@@ -11,4 +11,10 @@ where employee_id not in
 ( select e.employee_id 
 from Employees as e   join Salaries as s
 on e.employee_id = s.employee_id
-)
+)*/
+
+select isnull(e.employee_id, s.employee_id ) as employee_id
+from Employees as e full outer  join Salaries as s
+on e.employee_id = s.employee_id
+where name is null or salary is null 
+order by employee_id
