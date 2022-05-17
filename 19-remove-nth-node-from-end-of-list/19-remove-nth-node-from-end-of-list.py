ceab374cf7417c11a702644c86f4a372f8f57cec
omar khaled <omarkhald02@gmail.com>
2022-05-17 22:12:45 +0200

    Time: 72 ms (5.75%), Space: 13.8 MB (70.07%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -10,14 +10,16 @@ class Solution:
         while (temp):
             count += 1
             temp = temp.next
-        current = head
         if( not head or not head.next):
             return None
+        # handel case of [1,2]
         fast = head
         for i in range(0,n):
             fast = fast.next
         if not fast:                                            
             return head.next
+        # the main concept of solution 
+        current = head
         for i in range(count-n-1):
             current = current.next
         current.next = current.next.next
